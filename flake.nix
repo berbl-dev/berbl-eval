9a4caf100690c7c38c1662a63a3a1e5bddfbe153
David Pätzel <david.paetzel@posteo.de>
2022-01-24 16:23:23 +0100

    :snowflake: Update inputs, pin Python version

--- flake.nix
+++ flake.nix
@@ -1,24 +1,28 @@
 {
-  description = "A flake for building the berbl-eval library";
+  description = "The berbl-eval library";
 
+  # 2022-01-24
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
   inputs.pystan.url = "github:dpaetzel/flake-pystan-2.19.1.1";
   inputs.baycomp.url = "github:dpaetzel/flake-baycomp";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.overlays.url = "github:dpaetzel/overlays";
 
-  outputs = { self, nixpkgs, overlays, pystan, baycomp }: {
-
-    defaultPackage.x86_64-linux = with import nixpkgs {
+  outputs = { self, nixpkgs, overlays, pystan, baycomp }:
+    with import nixpkgs {
       system = "x86_64-linux";
-      overlays = with overlays.overlays; [ mlflow pandas ];
+      overlays = with overlays.overlays; [ mlflow ];
     };
-      python3.pkgs.buildPythonPackage rec {
+    let python = python39;
+    in rec {
+
+      defaultPackage.x86_64-linux = python.pkgs.buildPythonPackage rec {
         pname = "berbl-eval";
         version = "0.1.0";
 
         src = self;
 
-        propagatedBuildInputs = with python3.pkgs; [
+        propagatedBuildInputs = with python.pkgs; [
           baycomp.packages.x86_64-linux.baycomp
           mlflow
           numpy
@@ -36,5 +40,8 @@
           license = licenses.gpl3;
         };
       };
-  };
+      mlflowShell = mkShell {
+        packages = [ python.pkgs.mlflow ];
+      };
+    };
 }

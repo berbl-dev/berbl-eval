33adb3af2a0b747b22f6ed011f7988fda872b2c3
David Pätzel <david.paetzel@posteo.de>
2022-01-25 14:31:17 +0100

    :arrow_up: Update deps, fix numpy version issues

--- flake.nix
+++ flake.nix
@@ -4,33 +4,38 @@
   # 2022-01-24
   inputs.nixpkgs.url =
     "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
-  inputs.pystan.url = "github:dpaetzel/flake-pystan-2.19.1.1";
-  inputs.baycomp.url = "github:dpaetzel/flake-baycomp";
+  inputs.pystan.url = "github:dpaetzel/flake-pystan-2.19.1.1"; # numpy 1.21.2
+  inputs.baycomp.url = "github:dpaetzel/flake-baycomp"; # numpy 1.21.2
   inputs.overlays.url = "github:dpaetzel/overlays";
 
   outputs = { self, nixpkgs, overlays, pystan, baycomp }:
-    with import nixpkgs {
-      system = "x86_64-linux";
+    let system = "x86_64-linux";
+    in with import nixpkgs {
+      inherit system;
       overlays = with overlays.overlays; [ mlflow ];
     };
+
     let python = python39;
     in rec {
 
-      defaultPackage.x86_64-linux = python.pkgs.buildPythonPackage rec {
+      defaultPackage."${system}" = python.pkgs.buildPythonPackage rec {
         pname = "berbl-eval";
         version = "0.1.0";
 
         src = self;
 
         propagatedBuildInputs = with python.pkgs; [
-          baycomp.packages.x86_64-linux.baycomp
+          baycomp.packages."${system}".baycomp
           mlflow
-          numpy
-          numpydoc
+          numpy # 1.21.2
           pandas
-          pystan.packages.x86_64-linux.pystan
-          scipy
-          sphinx
+          pystan.packages."${system}".pystan
+
+          # having either of baycomp or pystan here breaks the build
+
+          # scipy
+          # numpydoc
+          # sphinx
         ];
 
         doCheck = false;
@@ -40,8 +45,8 @@
           license = licenses.gpl3;
         };
       };
-      mlflowShell = mkShell {
-        packages = [ python.pkgs.mlflow ];
-      };
+      mlflowShell = mkShell { packages = [ python.pkgs.mlflow ]; };
     };
 }
+
+# TODO Swap to setup.cfg pyproject.toml layout
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "baycomp",
         "mlflow ==1.22.0",
-        "numpy ==1.21.2",
+        "numpy ==1.21.4",
         "pandas ==1.3.4",
         "pystan ==2.19.1.1",
     ])

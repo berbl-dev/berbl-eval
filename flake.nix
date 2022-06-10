ecc37a7df658222308c4903f641c6681c6a94f79
David Pätzel <david.paetzel@posteo.de>
2022-06-10 15:44:05 +0200

    ⬆ Add dev shell, fix dependencies

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
 
   # 2022-01-24
   inputs.nixpkgs.url =
-    "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
+    # "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
+    # From cmpbayes, 2022-02-21.
+    "github:nixos/nixpkgs/7f9b6e2babf232412682c09e57ed666d8f84ac2d";
 
   inputs.baycomp.url = "github:dpaetzel/baycomp";
+  inputs.baycomp.inputs.nixpkgs.follows = "nixpkgs";
   inputs.cmpbayes.url = "github:dpaetzel/cmpbayes";
+  inputs.cmpbayes.inputs.nixpkgs.follows = "nixpkgs";
   inputs.overlays.url = "github:dpaetzel/overlays";
+  inputs.overlays.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, baycomp, cmpbayes, overlays }:
     let system = "x86_64-linux";
@@ -40,32 +45,12 @@
           matplotlib
           mlflow
           networkx
-          numpy # 1.21.2
+          numpy
           pandas
 
           python
           ipython
-
-          # We use ugly venvShellHook here because packaging pystan/httpstan is
-          # not entirely straightforward.
-          venvShellHook
         ];
-
-        venvDir = "./_venv";
-
-        postShellHook = ''
-          unset SOURCE_DATE_EPOCH
-
-          export LD_LIBRARY_PATH="${
-            lib.makeLibraryPath [ stdenv.cc.cc ]
-          }:$LD_LIBRARY_PATH";
-          PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
-        '';
-
-        postVenvCreation = ''
-          unset SOURCE_DATE_EPOCH
-          pip install pystan==3.4.0
-        '';
       };
 
       packages."${system}".fetser = stdenv.mkDerivation {
@@ -83,6 +68,37 @@
         propagatedBuildInputs = [
           python.pkgs.mlflow
         ];
+      };
+
+      devShell.${system} = pkgs.mkShell {
+
+        # We use ugly venvShellHook here because packaging pystan/httpstan is
+        # not entirely straightforward.
+        packages =
+          with python.pkgs;
+          [ ipython python venvShellHook ]
+          ++ [ defaultPackage."${system}"
+               packages."${system}".fetser
+             ];
+
+        venvDir = "./_venv";
+
+        postShellHook = ''
+          unset SOURCE_DATE_EPOCH
+
+          export LD_LIBRARY_PATH="${
+            pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]
+          }:$LD_LIBRARY_PATH";
+          PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
+        '';
+
+        # Using httpstan==4.7.2 (the default as of 2022-06-10) leads to a
+        # missing symbols error on NixOS. 4.7.1 works, however, so we use that.
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install httpstan==4.7.1 pystan==3.4.0
+        '';
+
       };
     };
 }
